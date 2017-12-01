b8c7dd7e79feee176a6ddb5165ff1212ad98de4b
Hemalatha <khema2010@gmail.com>
2017-11-30 16:14:31 -0800

    Update script.js

--- script.js
+++ script.js
@@ -13,9 +13,8 @@ var colorScale = d3.scale.quantile()
     .domain([0, 100])
     .range(colors);
 
-
-
 d3.select('#home').on('click', init);
+
 function buildtheHTML(build_array, instances) {
   var outer_html = '';
   var inner_a = '';
@@ -46,6 +45,7 @@ function buildtheHTML(build_array, instances) {
     }
   }
 }
+
 function init() {
   d3.selectAll('#sidebar ul li a').attr('aria-expanded', 'false');
   d3.selectAll('#sidebar ul li ul').attr('class', 'collapse list-unstyled');
@@ -62,6 +62,7 @@ function init() {
   }); 
 };
 init();
+
 var heatmapChart = function(instance, profile) {
   d3.selectAll('#sidebar ul li a').classed('active', false);
   d3.select('#chart').html('');
@@ -129,11 +130,14 @@ var heatmapChart = function(instance, profile) {
         .attr("transform", "translate(" + gridSize / 2 + ", -6)");
       
   // Prep the tooltip bits, initial display is hidden
-  var tip = d3.tip()
+  /*var tip = d3.tip()
     .attr('class', 'd3-tip')
     .html(function(d) {
       return "<span style='color:#333'>Available:" + (100 - d.value) + "%</span>";
     })
+
+  svg.call(tip);*/
+
 
   d3.select('#chart-heading').text(instance+'_'+profile);
   d3.select('#ins_val').text(instance);
@@ -174,7 +178,6 @@ var heatmapChart = function(instance, profile) {
             }
             else {
               current_floor_pu_values = Object.values(current_half_floor[i]);
-              //console.log(current_pu_values);
               var sum = (current_floor_pu_values).reduce(add, 0);
               var avg = sum/current_floor_pu_values.length;
               current_xLabel.push(avg);
@@ -186,22 +189,17 @@ var heatmapChart = function(instance, profile) {
           xValues.push(current_xLabel);
         }
       }
-      //console.log(xValues, pu_values);
-      //console.log(pu_values[0-17][0-3]); 
       return [xValues, pu_values];
     }
     for(var i = 0; i < instances.length; i++) {
       var profiles = Object.keys(d.FSL[instances[i]]); 
-      //console.log(profiles);
       if(instances[i] == instance) {
         var current_instance = Object.values(d.FSL[instances[i]]);
-
         for(var m = 0; m < current_instance.length; m++) {
           if(profiles[m] == profile) {
             var values = get_floor_values(current_instance[m]);
             xValues = values[0];
             pu_values = values[1];
-            console.log(xValues, pu_values);
             var jso = [], jso_val;
             xValues.forEach(function(d,i) {
               for(j = 0; j < d.length; j++) {
@@ -214,37 +212,29 @@ var heatmapChart = function(instance, profile) {
               }
             });
 
-            
-            svg.call(tip);
-
             var cards = svg.selectAll(".hour")
                 .data(jso, function(d) {return d.floor+':'+d.interfaces;});
 
-            //cards.append("title");
+            cards.enter().append("g")
+              .attr("class", "cards");
 
-            cards.enter().append("rect")
+
+            cards.append("rect")
                 .attr("x", function(d) { return (d.interfaces) * gridSize + 20; })
                 .attr("y", function(d) { return (d.floor) * gridSize/2 + 80; })
                 .attr('id', function(d) { return ("box" + d.floor + d.interfaces + '-' + d.value); })
                 .attr("rx", 2)
                 .attr("ry", 2)
                 .attr("class", "hour bordered")
-                .attr("width", gridSize/2)
-                .attr("height", gridSize/5)
-                .style("fill", function(d) { return colors[Math.floor(d.value/10)]; })
-                .on('mouseover', tip.show)
-                .on('mouseout', tip.hide)
+                .attr("width", gridSize)
+                .attr("height", gridSize/2)
+                .style("fill", function(d) { if(d.value == 100) { return colors[9];} else {return colors[Math.floor(d.value/10)];} })
+                .style('cursor', 'pointer')
+                //.on('mouseover', tip.show)
+                //.on('mouseout', tip.hide)
                 .on('click', handler);
-
-            cards.append("text")
-              .attr("class", "mono")
-              .text(function(d) { return d.value})
-              .attr("x", function(d) { return (d.interfaces) * gridSize + 20; })
-              .attr("y", function(d) { return (d.floor) * gridSize/2 + 80; })
-              .attr("transform", "translate(" + gridSize / 2 + ", -6)");
-
-                      
-            cards.transition()
+          
+            /*cards.transition()
                 .attr("rx", 4)
                 .attr("ry", 4)
                 .attr("width", gridSize)
@@ -252,9 +242,20 @@ var heatmapChart = function(instance, profile) {
                 .style("fill", function(d) { return colorScale(d.value); })
                 .delay(200)
                 .duration(500)
-                .ease("linear");
+                .ease("linear");*/
 
-            //cards.select("title").text(function(d) { return d.value; });
+            setTimeout(function(){
+              cards.append("text")
+                .attr('class', 'mono')
+                .text(function(d) { return d.value; })
+                .attr("x", function(d) { return (d.interfaces) * gridSize + gridSize/1.3; })
+                .attr("y", function(d) { return (d.floor) * gridSize/2 + 98; })
+                .style("fill", '#444') 
+                .style("font-size", "14px")
+                .style("font-weight", "Bold");
+              }, 300);
+
+            cards.select("title").text(function(d) { return d.value; });
 
             cards.exit().remove();
 
@@ -274,14 +275,13 @@ var heatmapChart = function(instance, profile) {
             legend.append("text")
               .attr("class", "mono")
               .text(function(d,i) { return "≥ " + Math.round(i*10); })
-              .attr("x", function(colors, i) { return (gridSize * i + 10); })
+              .attr("x", function(colors, i) { return (gridSize * i + 20); })
               .attr("y", height+gridSize);
 
             legend.exit().remove();
 
             function handler() {
               var box = this.id;
-              console.log(box);
               var value = box.split('-');
               d3.select('#chart-details')
                 .style('display', 'block');
@@ -375,17 +375,9 @@ var heatmapChart = function(instance, profile) {
                   ctx.canvas.height = "300px";
                   var arr = pu_values[Math.floor(floor)][interface-6];
                   var name = xsubLabel[interface];
-                  var color = ["#51C0BF", "#3DA3E8", "#FECC60", "#FD6585"];
-                  /*var arr_color = arr.map(function(val) {
-                    return color[Math.floor(val/25)];
-                  });*/
                   var inv_arr = arr.map(function(val) {
                     return 100-val;
                   });
-                  /*var inv_arr_color = inv_arr.map(function(val) {
-                    return color[Math.floor(val/25)];
-                  });*/
-                  //console.log(arr, arr_color);
                   draw_bar(ctx, arr, inv_arr, name);
                 }
               };
@@ -398,3 +390,193 @@ var heatmapChart = function(instance, profile) {
     }
   });
 };
+
+var busChart = function() {
+  var dataSetLabels, labels;
+  var colors = ["#6A496A","#9F5D57","#CE7442"];
+  var groupedBarData = [];
+  d3.json('bus.json', function(d) {
+    dataSetLabels = Object.keys(d);
+    for(var i = 0; i < dataSetLabels.length; i++) {
+      var current_utilization = d[dataSetLabels[i]]["Total-utilization"];
+      labels = Object.keys(current_utilization);
+      groupedBarData.push({
+        label: dataSetLabels[i],
+        backgroundColor: colors[i],
+        data: Object.values(current_utilization)
+      });
+    }
+    d3.select('#chart').html('');
+    d3.select('#chart').style('display', 'block'); 
+    d3.select('#chart-details').style('display', 'none');
+    d3.selectAll('#sidebar ul li a').classed('active', false);
+    d3.select('#bus').classed('active', true);
+    $('#chart').append('<canvas id="bar-chart-grouped" width="700" height="500"></canvas>');
+    var ctx = document.getElementById("bar-chart-grouped").getContext('2d');
+    var groupedBarChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: labels,
+        datasets: groupedBarData
+      },
+      options: {
+        title: {
+          display: true,
+          text: 'Bus Utilization',
+          fontColor: '#eee',
+          fontSize: 18,
+          fontFamily: "Consolas, courier"
+        }
+      }
+    });
+  });
+}
+
+var busHeatChart = function(id) {
+  d3.selectAll('#sidebar ul li a').classed('active', false);
+  d3.select('#'+id).classed('active', true);
+  d3.json('bus.json', function(d) {
+    var data = Object.values(d[id]);
+    data.splice(0,1)
+    //console.log(data);
+    d3.select('#chart').html('');
+    d3.select('#chart').style('display', 'block'); 
+    d3.select('#chart-details').style('display', 'none');
+
+    var yLabel = [], max = 0, xLabel, values = [];
+    var legend_arr = [0,1,2,3,4,5,6,7,8,9];
+    for(var i = 0; i < data.length; i++) {
+      yLabel.push('cmd-'+i);
+      var current_key = Object.keys(data[i]);
+      if(current_key.length > max) {
+        max = current_key.length;
+        xLabel = current_key;
+      }
+      values.push(Object.values(data[i]));
+    }
+
+    var jso = [], jso_val;
+    values.forEach(function(d,i) {
+      for(j = 0; j < d.length; j++) {
+        jso_val = { cmd : i,
+                    ifsl: j,
+                    value : d[j]
+                  }
+        jso.push(jso_val);
+      }
+    });
+
+    var svg = d3.select("#chart").append("svg")
+      .attr("width", width + margin.left + margin.right + 50)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var yLabels = svg.selectAll(".yLabel")
+        .data(yLabel)
+        .enter().append("text")
+          .text(function (d) { return d; })
+          .attr("x", -30)
+          .attr("y", function (d, i) { return i * gridSize/2 + 70; })
+          .style("text-anchor", "end")
+          .attr("transform", "translate(-6," + gridSize / 1.7 + ")")
+          .attr("class", "mono sub");
+
+    svg.append("text")
+      .attr("x", (width / 2))             
+      .attr("y", 0 - (margin.top / 250))
+      .attr("text-anchor", "middle")  
+      .style("font-size", "19px")
+      .style("font-weight", "Bold")
+      .style("font-color", "#999")
+      .attr("id", "chart-heading")  
+      .text("");
+
+
+    var xLabels = svg.selectAll(".xLabel")
+        .data(xLabel)
+        .enter().append("text")
+          .text(function(d) { return d; })
+          .attr("x", function(xLabel, i) { return i * 1.2 * gridSize; })
+          .attr("y", 50)
+          .style("text-anchor", "middle")
+          .attr("transform", "translate(" + gridSize / 2 + ", -6)")
+          .attr("class", "mono sub");
+        
+    // Prep the tooltip bits, initial display is hidden
+    /*var tip = d3.tip()
+      .attr('class', 'd3-tip')
+      .html(function(d) {
+        return "<span style='color:#333'>Available:" + (100 - d.value) + "%</span>";
+      })
+
+    svg.call(tip);*/
+
+    d3.select('#chart-heading').text(id+' Utilization');
+    
+    var cards = svg.selectAll(".grid")
+      .data(jso, function(d) {return d.cmd+':'+d.ifsl});
+
+    cards.enter().append("g")
+        .attr("class", "cards");
+
+    cards.append("rect")
+        .attr("x", function(d) { return (d.ifsl)*1.2 * gridSize ; })
+        .attr("y", function(d) { return (d.cmd) * gridSize/2 + 80; })
+        .attr('id', function(d) { return ("box" + d.floor + d.interfaces + '-' + d.value); })
+        .attr("rx", 2)
+        .attr("ry", 2)
+        .attr("class", "hour bordered")
+        .attr("width", gridSize*1.2)
+        .attr("height", gridSize/2)
+        .style("fill", function(d) { if(d.value == 100) { return colors[9];} else {return colors[Math.floor(d.value/10)];} });
+        //.on('mouseover', tip.show)
+        //.on('mouseout', tip.hide);
+        //.on('click', handler);
+
+    /*cards.transition()
+        .attr("rx", 4)
+        .attr("ry", 4)
+        .attr("width", gridSize*1.2)
+        .attr("height", gridSize/2)
+        .style("fill", function(d) { return colors[Math.floor(d.value/20)]; })
+        .delay(200)
+        .duration(500)
+        .ease("linear");*/
+
+    setTimeout(function(){
+      cards.append("text")
+        .attr('class', 'mono')
+        .text(function(d, i) { return d.value; })
+        .attr("x", function(d) { return (d.ifsl) * 1.2 * gridSize + gridSize/2; })
+        .attr("y", function(d) { return (d.cmd) * gridSize/2 + 95; })
+        .style("fill", '#444') 
+        .style("font-size", "14px")
+        .style("font-weight", "Bold");
+      }, 300);
+
+    cards.exit().remove();
+
+    var legend = svg.selectAll(".legend")
+      .data([0].concat(colorScale.quantiles()), function(d) { return d; });
+
+    legend.enter().append("g")
+        .attr("class", "legend");
+
+    legend.append("rect")
+      .attr("x", function(colors, i) { return gridSize * i - 10; })
+      .attr("y", height/1.4)
+      .attr("width", gridSize)
+      .attr("height", gridSize/2)
+      .style("fill", function(legend_arr, i) { return colors[i]; });
+
+    legend.append("text")
+      .attr("class", "mono")
+      .text(function(colors,i) { return "≥ " + Math.round(i*10); })
+      .attr("x", function(colors, i) { return (gridSize * i - 10); })
+      .attr("y", height/1.4+gridSize);
+
+    legend.exit().remove();    
+  });
+}
+d3.select('#bus').on('click', busChart);
